15cb175c528ef8470ea1a5e5d70935104e1f7316
Kelly Keating <keating.kel@gmail.com>
2022-09-19 17:41:11 +1200

    fetching imdb search

--- client/components/AddMovie.jsx
+++ client/components/AddMovie.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { searchForMovie } from '../apis/imdb'
 
 function AddMovie() {
+  const [movieSearch, setMovieSearch] = useState('')
+  const [results, setResults] = useState([])
+
+  const handleSearch = async (evt) => {
+    evt.preventDefault()
+    const searchResults = await searchForMovie(movieSearch)
+    setResults(searchResults)
+    setMovieSearch('')
+  }
+
+  const handleTyping = (evt) => {
+    setMovieSearch(evt.target.value)
+  }
+
   return (
     <>
       <h2>AddMovie</h2>
+
+      <form onSubmit={handleSearch} >
+        <label htmlFor='search'>New movie:</label>
+        <input type='text' id='search' onChange={handleTyping} value={movieSearch} />
+        <button>Search</button>
+      </form>
+
+      {results.map(movie => <p>{movie.title}</p>)}
     </>
   )
 }
