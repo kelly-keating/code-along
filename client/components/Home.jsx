929f727d3cfc982138cd3537c2c9209a1457869d
Kelly Keating <keating.kel@gmail.com>
2022-09-19 14:06:41 +1200

    movies showing from redux

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function Home() {
+  const movies = useSelector(store => store.movies)
+
   return (
     <>
       <h2>Home</h2>
+      <div className='movies'>
+      {movies.map(movie => {
+        return (
+          <div key={movie.id} className='movie__tile'>
+            <img src={movie.img} />
+            <p>{movie.title}</p>
+          </div>
+        )
+      })}
+      </div>
     </>
   )
 }
